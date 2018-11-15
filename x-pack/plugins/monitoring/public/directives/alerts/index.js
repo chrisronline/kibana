4bb47c6a00221fadd0bda289b3e9f87f060dd234
Chris Roberson <chrisronline@gmail.com>
2018-11-15 13:57:22 -0500

    Alerts table

--- x-pack/plugins/monitoring/public/directives/alerts/index.js
+++ x-pack/plugins/monitoring/public/directives/alerts/index.js
@@ -9,9 +9,8 @@ import React from 'react';
 import { render } from 'react-dom';
 import { EuiIcon, EuiHealth } from '@elastic/eui';
 import { uiModules } from 'ui/modules';
-import { KuiTableRowCell, KuiTableRow } from '@kbn/ui-framework/components';
 import { MonitoringTable } from 'plugins/monitoring/components/table';
-import { CALCULATE_DURATION_SINCE, SORT_DESCENDING } from '../../../common/constants';
+import { CALCULATE_DURATION_SINCE } from '../../../common/constants';
 import { Tooltip } from 'plugins/monitoring/components/tooltip';
 import { FormattedAlert } from 'plugins/monitoring/components/alerts/formatted_alert';
 import { mapSeverity } from 'plugins/monitoring/components/alerts/map_severity';
@@ -24,74 +23,88 @@ const linkToCategories = {
   'kibana/instances': 'Kibana Instances',
   'logstash/instances': 'Logstash Nodes',
 };
-const filterFields = [ 'message', 'severity_group', 'prefix', 'suffix', 'metadata.link', 'since', 'timestamp', 'update_timestamp' ];
-const columns = [
-  { title: 'Status', sortKey: 'metadata.severity', sortOrder: SORT_DESCENDING },
-  { title: 'Resolved', sortKey: 'resolved_timestamp' },
-  { title: 'Message', sortKey: 'message' },
-  { title: 'Category', sortKey: 'metadata.link' },
-  { title: 'Last Checked', sortKey: 'update_timestamp' },
-  { title: 'Triggered', sortKey: 'timestamp' },
-];
-const alertRowFactory = (scope, kbnUrl) => {
-  return props => {
-    const changeUrl = target => {
-      scope.$evalAsync(() => {
-        kbnUrl.changePath(target);
-      });
-    };
-    const severityIcon = mapSeverity(props.metadata.severity);
-    const resolution = {
-      icon: null,
-      text: 'Not Resolved'
-    };
+const getColumns = (kbnUrl, scope) => ([
+  {
+    name: 'Status',
+    field: 'metadata.severity',
+    render: severity => {
+      const severityIcon = mapSeverity(severity);
 
-    if (props.resolved_timestamp) {
-      resolution.text = `${formatTimestampToDuration(props.resolved_timestamp, CALCULATE_DURATION_SINCE)} ago`;
-    } else {
-      resolution.icon = <EuiIcon type="alert" size="m" aria-label="Not Resolved" />;
+      return (
+        <Tooltip text={severityIcon.title} placement="bottom" trigger="hover">
+          <EuiHealth color={severityIcon.color} data-test-subj="alertIcon" aria-label={severityIcon.title}>
+            { capitalize(severityIcon.value) }
+          </EuiHealth>
+        </Tooltip>
+      );
     }
+  },
+  {
+    name: 'Resolved',
+    field: 'resolved_timestamp',
+    render: (resolvedTimestamp) => {
+      const resolution = {
+        icon: null,
+        text: 'Not Resolved'
+      };
 
-    return (
-      <KuiTableRow>
-        <KuiTableRowCell tabIndex="0">
-          <Tooltip text={severityIcon.title} placement="bottom" trigger="hover">
-            <EuiHealth color={severityIcon.color} data-test-subj="alertIcon" aria-label={severityIcon.title}>
-              { capitalize(severityIcon.value) }
-            </EuiHealth>
-          </Tooltip>
-        </KuiTableRowCell>
-        <KuiTableRowCell tabIndex="0">
+      if (resolvedTimestamp) {
+        resolution.text = `${formatTimestampToDuration(resolvedTimestamp, CALCULATE_DURATION_SINCE)} ago`;
+      } else {
+        resolution.icon = <EuiIcon type="alert" size="m" aria-label="Not Resolved" />;
+      }
+
+      return (
+        <span>
           { resolution.icon } { resolution.text }
-        </KuiTableRowCell>
-        <KuiTableRowCell tabIndex="0">
-          <FormattedAlert
-            prefix={props.prefix}
-            suffix={props.suffix}
-            message={props.message}
-            metadata={props.metadata}
-            changeUrl={changeUrl}
-          />
-        </KuiTableRowCell>
-        <KuiTableRowCell tabIndex="0">
-          { linkToCategories[props.metadata.link] ? linkToCategories[props.metadata.link] : 'General' }
-        </KuiTableRowCell>
-        <KuiTableRowCell tabIndex="0">
-          { formatDateTimeLocal(props.update_timestamp) }
-        </KuiTableRowCell>
-        <KuiTableRowCell tabIndex="0">
-          { formatTimestampToDuration(props.timestamp, CALCULATE_DURATION_SINCE) } ago
-        </KuiTableRowCell>
-      </KuiTableRow>
-    );
-  };
-};
+        </span>
+      );
+    },
+  },
+  {
+    name: 'Message',
+    field: 'message',
+    render: (message, alert) => (
+      <FormattedAlert
+        prefix={alert.prefix}
+        suffix={alert.suffix}
+        message={message}
+        metadata={alert.metadata}
+        changeUrl={target => {
+          scope.$evalAsync(() => {
+            kbnUrl.changePath(target);
+          });
+        }}
+      />
+    )
+  },
+  {
+    name: 'Category',
+    field: 'metadata.link',
+    render: link => linkToCategories[link] ? linkToCategories[link] : 'General'
+  },
+  {
+    name: 'Last Checked',
+    field: 'update_timestamp',
+    render: timestamp => formatDateTimeLocal(timestamp)
+  },
+  {
+    name: 'Triggered',
+    field: 'timestamp',
+    render: timestamp => formatTimestampToDuration(timestamp, CALCULATE_DURATION_SINCE) + ' ago'
+  },
+]);
 
 const uiModule = uiModules.get('monitoring/directives', []);
 uiModule.directive('monitoringClusterAlertsListing', kbnUrl => {
   return {
     restrict: 'E',
-    scope: { alerts: '=' },
+    scope: {
+      alerts: '=',
+      pagination: '=paginationSettings',
+      sorting: '=',
+      onTableChange: '='
+    },
     link(scope, $el) {
 
       scope.$watch('alerts', (alerts = []) => {
@@ -99,10 +112,16 @@ uiModule.directive('monitoringClusterAlertsListing', kbnUrl => {
           <MonitoringTable
             className="alertsTable"
             rows={alerts}
-            placeholder="Filter Alerts..."
-            filterFields={filterFields}
-            columns={columns}
-            rowComponent={alertRowFactory(scope, kbnUrl)}
+            columns={getColumns(kbnUrl, scope)}
+            sorting={scope.sorting}
+            pagination={scope.pagination}
+            search={{
+              box: {
+                incremental: true,
+                placeholder: 'Filter Alerts...'
+              },
+            }}
+            onTableChange={scope.onTableChange}
           />
         );
         render(alertsTable, $el[0]);
